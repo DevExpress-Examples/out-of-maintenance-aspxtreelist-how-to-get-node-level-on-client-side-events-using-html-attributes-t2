d1ec9602be4f850973e285007654a5f1d319eab8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using DevExpress.Web.ASPxTreeList;
+using DevExpress.Web.ASPxTreeList.Internal;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+
+    }
+
+    protected void ASPxTreeList1_HtmlRowPrepared(object sender, TreeListHtmlRowEventArgs e) {
+        e.Row.Attributes.Add("nodelevel", e.Level.ToString());
+    }
+}
---
+++ VB/Default.aspx.vb
@@ -0,0 +1,21 @@
+﻿Imports DevExpress.Web.ASPxTreeList
+Imports DevExpress.Web.ASPxTreeList.Internal
+Imports System
+Imports System.Collections.Generic
+Imports System.Drawing
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+    End Sub
+
+    Protected Sub ASPxTreeList1_HtmlRowPrepared(ByVal sender As Object, ByVal e As TreeListHtmlRowEventArgs)
+        e.Row.Attributes.Add("nodelevel", e.Level.ToString())
+    End Sub
+End Class
